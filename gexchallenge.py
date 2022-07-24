06f5bee90698c1eb6e3cc1e303d15fb29489c3c1
333pranav <pranav03.sharma@gmail.com>
2022-07-24 14:25:26 +0530

    Adding Files

---
+++ gexchallenge.py
@@ -0,0 +1,16 @@
+import re
+
+f = open('BLR Flight Schedule.csv', 'r')
+
+departures = []
+f.readline()
+
+for s in f.readlines():
+    if(re.search('Departure', s)):
+        departures.append(s)
+
+for s in departures:
+    info = re.findall(r'(^\d\d-\d\d-\d\d\d\d,\w+ \w+,[\w | ]+,)Departure,BLR,Bengaluru,Kempegowda International Airport,\d\d-\d\d-\d\d\d\d', s)
+    print(info)
+    
+f.close()
